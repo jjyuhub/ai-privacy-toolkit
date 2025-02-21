7f08232460a5ce7801051cf7991b2fc192d3dfcb
jjyuhub <jyu290@student.vu.nl>
2025-02-21 18:21:34 +0100

    Update test_minimalistic.py

--- tests/test_minimalistic.py
+++ tests/test_minimalistic.py
@@ -27,10 +27,192 @@ from apt.utils.datasets import ArrayDataset
 from apt.utils.models import SklearnClassifier, SklearnRegressor, KerasClassifier, \
     CLASSIFIER_SINGLE_OUTPUT_CLASS_PROBABILITIES, CLASSIFIER_SINGLE_OUTPUT_CATEGORICAL, \
     CLASSIFIER_SINGLE_OUTPUT_CLASS_LOGITS, CLASSIFIER_MULTI_OUTPUT_BINARY_LOGITS
-
 tf.compat.v1.disable_eager_execution()
 
-ACCURACY_DIFF = 0.05  # Maximum allowed accuracy difference after anonymization
+
+ACCURACY_DIFF = 0.05
+
+
+@pytest.fixture
+def diabetes_dataset():
+    return load_diabetes()
+
+
+@pytest.fixture
+def cells():
+    cells = [{"id": 1, "ranges": {"age": {"start": None, "end": 38}, "height": {"start": None, "end": 170}}, "label": 0,
+              'categories': {}, "representative": {"age": 26, "height": 149}},
+             {"id": 2, "ranges": {"age": {"start": 39, "end": None}, "height": {"start": None, "end": 170}}, "label": 1,
+              'categories': {}, "representative": {"age": 58, "height": 163}},
+             {"id": 3, "ranges": {"age": {"start": None, "end": 38}, "height": {"start": 171, "end": None}}, "label": 0,
+              'categories': {}, "representative": {"age": 31, "height": 184}},
+             {"id": 4, "ranges": {"age": {"start": 39, "end": None}, "height": {"start": 171, "end": None}}, "label": 1,
+              'categories': {}, "representative": {"age": 45, "height": 176}}
+             ]
+    features = ['age', 'height']
+    x = np.array([[23, 165],
+                  [45, 158],
+                  [18, 190]])
+    y = [1, 1, 0]
+    return cells, features, x, y
+
+
+@pytest.fixture
+def cells_categorical():
+    cells = [{'id': 1, 'label': 0, 'ranges': {'age': {'start': None, 'end': None}},
+              'categories': {'sex': ['f', 'm']}, 'hist': [2, 0],
+              'representative': {'age': 45, 'height': 149, 'sex': 'f'},
+              'untouched': ['height']},
+             {'id': 3, 'label': 1, 'ranges': {'age': {'start': None, 'end': None}},
+              'categories': {'sex': ['f', 'm']}, 'hist': [0, 3],
+              'representative': {'age': 23, 'height': 165, 'sex': 'f'},
+              'untouched': ['height']},
+             {'id': 4, 'label': 0, 'ranges': {'age': {'start': None, 'end': None}},
+              'categories': {'sex': ['f', 'm']}, 'hist': [1, 0],
+              'representative': {'age': 18, 'height': 190, 'sex': 'm'},
+              'untouched': ['height']}
+             ]
+    features = ['age', 'height', 'sex']
+    x = [[23, 165, 'f'],
+         [45, 158, 'f'],
+         [56, 123, 'f'],
+         [67, 154, 'm'],
+         [45, 149, 'f'],
+         [42, 166, 'm'],
+         [73, 172, 'm'],
+         [94, 168, 'f'],
+         [69, 175, 'm'],
+         [24, 181, 'm'],
+         [18, 190, 'm']]
+    y = np.array([1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0])
+    return cells, features, x, y
+
+
+@pytest.fixture
+def data_two_features():
+    x = np.array([[23, 165],
+                  [45, 158],
+                  [56, 123],
+                  [67, 154],
+                  [45, 149],
+                  [42, 166],
+                  [73, 172],
+                  [94, 168],
+                  [69, 175],
+                  [24, 181],
+                  [18, 190]])
+    y = np.array([1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0])
+    x1 = np.array([[33, 165],
+                   [43, 150],
+                   [71, 143],
+                   [92, 194],
+                   [13, 125],
+                   [22, 169]])
+    features = ['age', 'height']
+    return x, y, features, x1
+
+
+@pytest.fixture
+def data_three_features():
+    features = ['age', 'height', 'weight']
+    x = np.array([[23, 165, 70],
+                  [45, 158, 67],
+                  [56, 123, 65],
+                  [67, 154, 90],
+                  [45, 149, 67],
+                  [42, 166, 58],
+                  [73, 172, 68],
+                  [94, 168, 69],
+                  [69, 175, 80],
+                  [24, 181, 95],
+                  [18, 190, 102]])
+    y = np.array([1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0])
+    return x, y, features
+
+
+@pytest.fixture
+def data_four_features():
+    features = ['age', 'height', 'sex', 'ola']
+    x = [[23, 165, 'f', 'aa'],
+         [45, 158, 'f', 'aa'],
+         [56, 123, 'f', 'bb'],
+         [67, 154, 'm', 'aa'],
+         [45, 149, 'f', 'bb'],
+         [42, 166, 'm', 'bb'],
+         [73, 172, 'm', 'bb'],
+         [94, 168, 'f', 'aa'],
+         [69, 175, 'm', 'aa'],
+         [24, 181, 'm', 'bb'],
+         [18, 190, 'm', 'bb']]
+    y = np.array([1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0])
+    x1 = [[33, 165, 'f', 'aa'],
+          [43, 150, 'm', 'aa'],
+          [71, 143, 'f', 'aa'],
+          [92, 194, 'm', 'aa'],
+          [13, 125, 'f', 'aa'],
+          [22, 169, 'f', 'bb']]
+    return x, y, features, x1
+
+
+@pytest.fixture
+def data_five_features():
+    features = ['age', 'height', 'weight', 'sex', 'ola']
+    x = [[23, 165, 65, 'f', 'aa'],
+         [45, 158, 76, 'f', 'aa'],
+         [56, 123, 78, 'f', 'bb'],
+         [67, 154, 87, 'm', 'aa'],
+         [45, 149, 45, 'f', 'bb'],
+         [42, 166, 76, 'm', 'bb'],
+         [73, 172, 85, 'm', 'bb'],
+         [94, 168, 92, 'f', 'aa'],
+         [69, 175, 95, 'm', 'aa'],
+         [24, 181, 49, 'm', 'bb'],
+         [18, 190, 69, 'm', 'bb']]
+    y = pd.Series([1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0])
+    return x, y, features
+
+
+def compare_generalizations(gener, expected_generalizations):
+    for key in expected_generalizations['ranges']:
+        assert (set(expected_generalizations['ranges'][key]) == set(gener['ranges'][key]))
+    for key in expected_generalizations['categories']:
+        assert (set([frozenset(sl) for sl in expected_generalizations['categories'][key]])
+                == set([frozenset(sl) for sl in gener['categories'][key]]))
+    assert (set(expected_generalizations['untouched']) == set(gener['untouched']))
+    if 'range_representatives' in expected_generalizations:
+        for key in expected_generalizations['range_representatives']:
+            assert (set(expected_generalizations['range_representatives'][key])
+                    == set(gener['range_representatives'][key]))
+    if 'category_representatives' in expected_generalizations:
+        for key in expected_generalizations['category_representatives']:
+            assert (set(expected_generalizations['category_representatives'][key])
+                    == set(gener['category_representatives'][key]))
+
+
+def check_features(features, expected_generalizations, transformed, x, pandas=False):
+    modified_features = [f for f in features if
+                         f in expected_generalizations['categories'].keys() or f in expected_generalizations[
+                             'ranges'].keys()]
+
+    if pandas:
+        np.testing.assert_array_equal(transformed.drop(modified_features, axis=1), x.drop(modified_features, axis=1))
+        if len(expected_generalizations['ranges'].keys()) > 0 or len(expected_generalizations['categories'].keys()) > 0:
+            assert (((transformed[modified_features]).equals(x[modified_features])) is False)
+    else:
+        indexes = []
+        for i in range(len(features)):
+            if features[i] in modified_features:
+                indexes.append(i)
+        if len(indexes) != transformed.shape[1]:
+            assert (np.array_equal(np.delete(transformed, indexes, axis=1), np.delete(x, indexes, axis=1)))
+        if len(expected_generalizations['ranges'].keys()) > 0 or len(expected_generalizations['categories'].keys()) > 0:
+            assert (not np.array_equal(transformed[:, indexes], x[:, indexes]))
+
+
+def check_ncp(ncp, expected_generalizations):
+    if len(expected_generalizations['ranges'].keys()) > 0 or len(expected_generalizations['categories'].keys()) > 0:
+        assert (ncp > 0.0)
+
 
 def test_minimizer_fit(data_two_features):
     """
